17c40c07f5b5cdfaafa688943fe536ba6d788f8f
Kjetil Helland <kjetil@hugintech.com>
2017-10-22 12:59:35 +0300

    Removed spine from cocos2d-x android build

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -303,7 +303,6 @@ LOCAL_MODULE_FILENAME := libcocos2d
 LOCAL_STATIC_LIBRARIES := cocostudio_static
 LOCAL_STATIC_LIBRARIES += cocosbuilder_static
 LOCAL_STATIC_LIBRARIES += cocos3d_static
-LOCAL_STATIC_LIBRARIES += spine_static
 LOCAL_STATIC_LIBRARIES += cocos_network_static
 LOCAL_STATIC_LIBRARIES += audioengine_static
 
@@ -322,7 +321,6 @@ $(call import-module,3d)
 $(call import-module,audio/android)
 $(call import-module,editor-support/cocosbuilder)
 $(call import-module,editor-support/cocostudio)
-$(call import-module,editor-support/spine)
 $(call import-module,network)
 $(call import-module,ui)
 $(call import-module,extensions)
